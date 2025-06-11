ff4a65d6d0d5e4ba0adc50d52c15e8c86121cc7f
bittucse <bittucse@outlook.com>
2025-06-11 23:13:38 +0530

    fix styles

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -62,7 +62,7 @@ const CompanionForm = () => {
 
   return (
      <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 mb-10">
         <FormField
           control={form.control}
           name="name"
