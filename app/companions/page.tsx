14c539dd4de8f2dcb68f29986e258c8294ae2d16
bittucse <bittucse@outlook.com>
2025-06-14 09:52:18 +0530

    add fetch companions from supabase

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,9 +1,21 @@
+// this shows all the info about the created companions
 
-const CompanionsLibrary = () => {
+import { getAllCompanions } from "@/lib/actions/companion.action";
+
+const CompanionsLibrary = async({searchParams}:SearchParams) => {
+  const filter= await searchParams; // This will be the search params passed to the page serachParams is a Next.js feature that allows you to access the query parameters in the URL.
+
+  const subject=filter.subject? filter.subject : '';
+  const topic=filter.topic? filter.topic : '';
+
+  const companions= await getAllCompanions({subject, topic});
+
+  console.log("Companions:", companions);
   return (
-    <div>
-      This is the Companions Library page.
-    </div>
+    <section className=" flex justify-between gap-4 max-sm:flex-col">
+      <h1>Companions Library</h1>
+      <div className=" flex gap-4"> fliters</div>
+    </section>
   )
 }
 
