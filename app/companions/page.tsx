2e2c2296b82328220da369871923d34b61cff3c4
bittucse <bittucse@outlook.com>
2025-06-16 12:05:59 +0530

    implement search flilters

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,6 +1,10 @@
 // this shows all the info about the created companions
 
+import CompanionCard from "@/components/CompanionCard";
+import SearchInput from "@/components/SearchInput";
+import SubjectFilter from "@/components/SubjectFilter";
 import { getAllCompanions } from "@/lib/actions/companion.action";
+import { getSubjectColor } from "@/lib/utils";
 
 const CompanionsLibrary = async({searchParams}:SearchParams) => {
   const filter= await searchParams; // This will be the search params passed to the page serachParams is a Next.js feature that allows you to access the query parameters in the URL.
@@ -12,10 +16,22 @@ const CompanionsLibrary = async({searchParams}:SearchParams) => {
 
   console.log("Companions:", companions);
   return (
+    <main>
+
     <section className=" flex justify-between gap-4 max-sm:flex-col">
       <h1>Companions Library</h1>
-      <div className=" flex gap-4"> fliters</div>
+      <div className=" flex gap-4"> 
+        <SearchInput/>
+        <SubjectFilter/>
+      </div>
     </section>
+    <section className="companions-grid">
+      {companions.map((companion)=>(
+        <CompanionCard key={companion.id} {...companion} color={getSubjectColor(companion.subject)}/>
+
+      ))}
+    </section>
+    </main>
   )
 }
 
