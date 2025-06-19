2c6265fbda06988ddd9f734d2be076e2fec9ee9b
bittucse <bittucse@outlook.com>
2025-06-19 09:50:52 +0530

    implement Callstatus animations

--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -1,7 +1,12 @@
 'use client';
 
+// whenever you use invent handlers in useEffect, make sure to clean them up to optimize performance and avoid memory leaks
+
 import { cn, getSubjectColor } from '@/lib/utils'
-import React, { useState } from 'react'
+import { vapi } from '@/lib/vapi.sdk';
+import Lottie from 'lottie-react';
+import Image from 'next/image';
+import React, { useEffect, useState } from 'react'
 
 enum CallStatus {
     INACTIVE='INACTIVE',
@@ -14,15 +19,49 @@ enum CallStatus {
 const CompanionComponent = ({companionId, subject,topic, name,userName,userImage,style,voice}:CompanionComponentProps) => {
 
     const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
+    const [isSpeaking, setIsSpeaking] = useState(false)
 
+    useEffect(() => {
+      const onCallStart=()=> setCallStatus(CallStatus.ACTIVE);
+
+      const onCallEnd=()=> setCallStatus(CallStatus.FINISHED);
+
+      const onMessage=()=>{}
+
+      const onSpeechStart=()=>setIsSpeaking(true);
+      const onSpeechEnd=()=>setIsSpeaking(false);
+
+      const onError=(error:Error)=> console.log('Error:', error);
+
+      vapi.on('call-start', onCallStart);
+      vapi.on('call-end', onCallEnd);
+      vapi.on('message', onMessage);
+      vapi.on('speech-start', onSpeechStart);
+      vapi.on('speech-end', onSpeechEnd);
+
+      return () => {
+        vapi.off('call-start', onCallStart);
+        vapi.off('call-end', onCallEnd);
+        vapi.off('message', onMessage);
+        vapi.off('speech-start', onSpeechStart);
+        vapi.off('speech-end', onSpeechEnd);
+        vapi.off('error', onError);
+      }
+    },[])
 
   return (
     <section className='flex flex-col h-[70vh]'>
       <section className='flex gap-8 max-sm:flex-col'>
         <div className='companion-section'>
             <div className='companion-avatar' style={{backgroundColor: getSubjectColor(subject)}}>
-                <div className={cn('absolute transition-opacity duration-1000')}>
+                <div className={cn('absolute transition-opacity duration-1000', callStatus === CallStatus.FINISHED || callStatus === CallStatus.INACTIVE ?'opacity-1001':'opacity-0', callStatus === CallStatus.CONNECTING && 'opacity-100 animate-pulse')}>
+                    <Image src={`/icons/${subject}.svg`} alt={subject} width={150} height={150} className='max-sm:w-fit'/>
+                </div>
 
+                <div className={cn('absolute transition-opacity duration-1000', callStatus === CallStatus.ACTIVE ?'opacity-100':'opacity-0')}>
+                    {/* <Lottie
+                    lottieRef={lottieRef}
+                    /> */}
                 </div>
             </div>
         </div>
