1988011a9302de6a4910493c0642fceddacbd188
bittucse <bittucse@outlook.com>
2025-06-28 09:31:45 +0530

    add session history

--- lib/actions/companion.action.ts
+++ lib/actions/companion.action.ts
@@ -2,6 +2,7 @@
 
 import { auth } from "@clerk/nextjs/server"
 import { createSupabaseClient } from "../supabase";
+import { tr } from "zod/v4/locales";
 
 export const createCompanion= async (formData: CreateCompanion)=>{
     const {userId:author}= await auth();
@@ -54,4 +55,33 @@ export const getCompanion= async (id:string)=>{
     if(error) return console.log(error);
 
     return data[0];
+}
+
+export const addToSessionHistory= async (companionId:string)=>{
+    const {userId}= await auth();
+    const supabase=createSupabaseClient();
+    const { data, error } = await supabase.from('session_history').insert({
+        companion_id: companionId,
+        user_id: userId,
+    });
+
+    if(error){
+        throw new Error(error.message);
+    }
+
+    return data;
+}
+
+
+export const getRecentSessions= async ({limit=10})=>{
+    const supabase=createSupabaseClient();
+    const {data,error}= await supabase
+    .from('session_history')
+        .select(`companions:companion_id(*)`)
+        .order('created_at', { ascending: false })
+        .limit(limit)
+    
+    if(error) throw new Error(error.message);
+
+    return data.map(({companions}) => companions);
 }
