839a9300abe87d59831e304ef6e6bd8cbdcb2a04
bittucse <bittucse@outlook.com>
2025-07-03 19:40:50 +0530

    fix static page error

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -16,7 +16,7 @@ const CompanionSession = async ({params}: CompanionSessionpageProps) => {
   const {id}= await params;
   const companion = await getCompanion(id);
   const user = await currentUser();
-  const {name, subject, title, topic, duration}= companion;
+  const {name, subject, topic, duration}= companion;
   if(!user) redirect('/sign-in');
   if(!companion) redirect('/companions');
 
@@ -27,7 +27,7 @@ const CompanionSession = async ({params}: CompanionSessionpageProps) => {
         <div className=" size-[72px] flex items-center justify-center rounded-lg max-md:hidden" style={
           {backgroundColor: getSubjectColor(subject)}
         }>
-          <Image src={`/icons/${subject}.svg`}  alt={subject+title} width={35} height={35} />
+          <Image src={`/icons/${subject}.svg`}  alt={subject} width={35} height={35} />
         </div>
         <div className=" flex flex-col gap-2">
           <div className="flex items-center gap-2">
